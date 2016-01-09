b4af4808b34e21f904e772c0bef170eac5853658
saintpablo94 <saint.pablo94@gmail.com>
2016-01-09 21:59:01 +0900

    https://youtu.be/J6NgljnfXoQ?list=PLG7te9eYUi7vXZf7O6Fd2YCnJlx5YG9qq 여기서부터 보기 08 04

---
+++ chapter02/MyIntent/app/src/main/java/org/androidtown/myintent/MainActivity.java
@@ -0,0 +1,71 @@
+package org.androidtown.myintent;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int ACTIVITY_MENU = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onButton1Clicked(View v){
+//      Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:010-3722-2035"));
+//      startActivity(intent);
+
+//      Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+//      startActivityForResult(intent, ACTIVITY_MENU);
+
+        Intent intent = new Intent();
+        ComponentName name = new ComponentName(
+                "org.androidtown.myintent",
+                "org.androidtown.myintent.MenuActivity");
+        intent.setComponent(name);
+        startActivityForResult(intent, ACTIVITY_MENU);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
